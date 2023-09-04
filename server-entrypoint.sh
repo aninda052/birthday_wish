2d6a982f60370fffd891d6200585891e91a870c6
aninda052 <anindahalder052@gmail.com>
2023-09-05 02:01:16 +0600

    create initial superuser on docker container

--- server-entrypoint.sh
+++ server-entrypoint.sh
@@ -2,6 +2,10 @@
 
 python manage.py makemigrations
 python manage.py migrate --no-input
-#python manage.py collectstatic --noinput
+
+# create initial superuser.
+# Login to Django admin panel, create a new user with
+# superuser status and delete this initial superuser
+DJANGO_SUPERUSER_PASSWORD=admin DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_EMAIL=admin@email.com python manage.py createsuperuser --noinput
 
 python manage.py runserver 0.0.0.0:8000
